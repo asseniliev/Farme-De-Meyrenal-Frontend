7eff4ffd9d3ea084de2e1e088cdd50e084a7fafa
Jason <jason.ricou@gmail.com>
2023-04-04 12:22:36 +0200

    MyOrdersScreen fixed

--- screens/profilScreens/MyOrdersScreen.js
+++ screens/profilScreens/MyOrdersScreen.js
@@ -15,6 +15,7 @@ import { AntDesign } from "@expo/vector-icons";
 export default function MyOrders({ navigation }) {
   const [orderList, setOrderList] = useState([]);
   const [isOpen, setIsOpen] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const [fontsLoaded] = useFonts({
     BelweBold: require("../../assets/fonts/BelweBold.otf"),
   });
@@ -24,7 +25,13 @@ export default function MyOrders({ navigation }) {
     fetch(`${backendUrl}/orders/?user=${loggedUser.id}`)
       .then((response) => response.json())
       .then((data) => {
-        setOrderList(data.result);
+        console.log("data", data);
+        if (data.result) setOrderList(data.result);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log("error", error);
+        setIsLoading(false);
       });
   }, [navigation]);
 
@@ -37,65 +44,90 @@ export default function MyOrders({ navigation }) {
     setIsOpen({ ...isOpen, [key]: !isOpen[key] });
   }
 
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          {"\n"}
+          {"\n"}
+          {"\n"} Chargement en cours...
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         {fontsLoaded && <Text style={styles.title}>Mes commandes</Text>}
       </View>
       <ScrollView style={styles.scrollView}>
-        <View style={{ height: 30 }}></View>
-        <View style={styles.ordersContainer}>
-          {orderList
-            .map((order, i) => (
-              <View style={styles.orderContainer} key={i}>
-                <Text style={styles.text}>
-                  Date de commande: {formatDate(order.date)}
-                </Text>
-                <Text style={styles.text}>Numéro : {order.orderNumber}</Text>
-                <Text style={styles.text}>
-                  Montant total : {order.totalAmount}
-                </Text>
-                <Text style={styles.text}>
-                  Payé : {order.isPaid ? "Oui" : "Non"}
-                </Text>
-                <TouchableOpacity
-                  onPress={() => toggleOrderDetails(i)}
-                  style={styles.dropDownButton}
-                >
-                  <Text style={importedStyle.textButton}>
-                    Détails de la commande :{"             "}
-                  </Text>
-                  <Text>
-                    <AntDesign
-                      name={isOpen[i] ? "up" : "down"}
-                      size={24}
-                      color="white"
-                    />
-                  </Text>
-                </TouchableOpacity>
-                {isOpen[i] && (
-                  <View style={styles.dropDownMenu}>
-                    {order.items.map((item, j) => (
-                      <View key={j} style={{ marginBottom: 3 }}>
-                        <Text style={styles.text}>
-                          {" "}
-                          {item.title || "  Titre du produit non disponible"}
-                        </Text>
-                        <Text style={styles.detailsText}>
-                          {"   "}
-                          Quantité : {item.quantity} {item.priceUnit}
-                        </Text>
-                        <Text style={styles.detailsText}>
-                          {"   "}Prix unitaire : {item.price}€
-                        </Text>
+        {orderList.length === 0 ? (
+          <View style={styles.orderContainer}>
+            <Text style={styles.text}>
+              Pas de commandes passées pour l'instant
+            </Text>
+          </View>
+        ) : (
+          <>
+            <View style={{ height: 30 }}></View>
+            <View style={styles.ordersContainer}>
+              {orderList
+                .map((order, i) => (
+                  <View style={styles.orderContainer} key={i}>
+                    <Text style={styles.text}>
+                      Date de commande: {formatDate(order.date)}
+                    </Text>
+                    <Text style={styles.text}>
+                      Numéro : {order.orderNumber}
+                    </Text>
+                    <Text style={styles.text}>
+                      Montant total : {order.totalAmount}
+                    </Text>
+                    <Text style={styles.text}>
+                      Payé : {order.isPaid ? "Oui" : "Non"}
+                    </Text>
+                    <TouchableOpacity
+                      onPress={() => toggleOrderDetails(i)}
+                      style={styles.dropDownButton}
+                    >
+                      <Text style={importedStyle.textButton}>
+                        Détails de la commande :{"             "}
+                      </Text>
+                      <Text>
+                        <AntDesign
+                          name={isOpen[i] ? "up" : "down"}
+                          size={24}
+                          color="white"
+                        />
+                      </Text>
+                    </TouchableOpacity>
+                    {isOpen[i] && (
+                      <View style={styles.dropDownMenu}>
+                        {order.items.map((item, j) => (
+                          <View key={j} style={{ marginBottom: 3 }}>
+                            <Text style={styles.text}>
+                              {" "}
+                              {item.title ||
+                                "  Titre du produit non disponible"}
+                            </Text>
+                            <Text style={styles.detailsText}>
+                              {"   "}
+                              Quantité : {item.quantity} {item.priceUnit}
+                            </Text>
+                            <Text style={styles.detailsText}>
+                              {"   "}Prix unitaire : {item.price}€
+                            </Text>
+                          </View>
+                        ))}
                       </View>
-                    ))}
+                    )}
                   </View>
-                )}
-              </View>
-            ))
-            .reverse()}
-        </View>
+                ))
+                .reverse()}
+            </View>
+          </>
+        )}
       </ScrollView>
     </View>
   );
