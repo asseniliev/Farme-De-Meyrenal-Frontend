b2fdd3973f6bc0f96818b1a0ea5401feebaed1f0
Jason <jason.ricou@gmail.com>
2023-05-31 17:53:03 +0200

    dashboardscreen not exist

--- App.js
+++ App.js
@@ -40,7 +40,7 @@ import PasswordChangeScreen from "./screens/profilScreens/PasswordChangeScreen";
 import PasswordChangeSuccessScreen from "./screens/profilScreens/PasswordChangeSuccessScreen";
 
 // adminScreens
-import DashboardScreen from "./screens/adminScreens/Dashboard";
+import Dashboard from "./screens/adminScreens/Dashboard";
 import CheckingOrdersScreen from "./screens/adminScreens/CheckingOrdersScreen";
 import RoadmapScreen from "./screens/adminScreens/RoadmapScreen";
 import BasketPrepScreen from "./screens/adminScreens/BasketPrepScreen";
@@ -310,7 +310,7 @@ export default function App() {
             />
 
             <Stack.Screen name="ListOfProducts" component={ListOfProducts} />
-            <Stack.Screen name="Dashboard" component={DashboardScreen} />
+            <Stack.Screen name="Dashboard" component={Dashboard} />
             <Stack.Screen name="ProductDetails" component={ProductDetails} />
             <Stack.Screen name="ProductCreated" component={ProductCreated} />
             <Stack.Screen name="SnapScreen" component={SnapScreen} />
